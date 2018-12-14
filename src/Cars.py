861d3270f04232bc0fa75363e6b2e408bc57d102
lgosek <luki2012@gmail.com>
2018-12-14 15:16:17 +0100

    Dodanie posY do Car (Cars.py)

--- src/Cars.py
+++ src/Cars.py
@@ -1,10 +1,27 @@
 import random
 
+CARSIZE = 1
+SPACING = 5
+
+def convertY (Y):
+    return 400-Y
+
 class Car:
-    def __init__(self, gridSize, lane, carNum):
+    def __init__(self, gridSize, lane, carNum, canvas):
         self.posX = random.randint(0, gridSize)
         self.posY = lane
         self.maxVel = random.randint(2,5)
         self.currentVel = 0
         self.id = carNum
+        self.canvas = canvas
+        self.laneChange = 0
+        cx = (self.posX)
+        cy = convertY(SPACING + self.posY*(CARSIZE+SPACING))-CARSIZE
+        self.marker = canvas.create_rectangle(cx, cy, cx+CARSIZE, cy+CARSIZE, fill="red", tags=)
+
+    def updateMarker(self):
+        print(self.id)
+        curPosX = self.canvas.coords(self.marker)[0]
+        shiftX = self.posX - curPosX
+        self.canvas.move(self.marker, shiftX, self.laneChange*(CARSIZE+SPACING))
 
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import Cars
 import random
+from tkinter import *
 from time import sleep
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,12 +13,12 @@ TIME = 500
 #Liczba samochodów
 CARNUM = 20
 #Liczba pasów ruchu
-LANES = 3
+LANES = 2
 #Maksymalna prędkość
 MAXVEL = 5
 
 #liczba wyroznionych samochodow
-HIGHLIGHTED = 10;
+HIGHLIGHTED = 10
 
 
 
@@ -28,37 +29,18 @@ def OrderCars (car):
     return car.posX
 
 class Model:
-    def __init__(self):
+    def __init__(self, tk, canvas):
+        self.tk = tk
+        self.canvas = canvas
+
         # generowanie samochodów
-        #numRight = round(random.random()*CARNUM)
-        # traffic[0] - prawy pas, traffic[1] - lewy pas
-        #self.traffic = [[Cars.Car(GRIDLEN, 0) for i in range(0, numRight)], [Cars.Car(GRIDLEN, 1) for i in range(0, CARNUM-numRight)]]
-        self.traffic = [ [Cars.Car(GRIDLEN, j, j*CARNUM+i) for i in range(0, CARNUM)]  for j in range(LANES) ]
-        #self.traffic = [Cars.Car(GRIDLEN, 0) for i in range(0, CARNUM)]
+        self.traffic = [ [Cars.Car(GRIDLEN, j, j*CARNUM+i,canvas) for i in range(0, CARNUM)]  for j in range(LANES) ]
+
         # ułożenie samochodów na siatce rosnąco względem posX
         for i in range (LANES):
             self.traffic[i].sort(key=OrderCars)
-        # self.traffic[0].sort(key=OrderCars)
-        # self.traffic[1].sort(key=OrderCars)
-        #self.traffic.sort(key=OrderCars)
 
-        self.fig, self.ax = plt.subplots()
-        self.x = np.arange(0, GRIDLEN, 1)
-
-        self.highlightID = random.sample(range(0, CARNUM*LANES), HIGHLIGHTED)
-        grd = self.printGrid()
-
-
-
-        # self.line0 = self.ax.plot(self.x, grd[0], marker='.', linestyle='', color='b')[0]
-        # self.line1 = self.ax.plot(self.x, grd[1], marker='.', linestyle='', color='b')[0]
-
-        self.lines = [self.ax.plot(self.x, grd[i], marker='.', linestyle='', color='b')[0] for i in range(LANES)]
-        self.lines.append(self.ax.plot(self.x, grd[LANES], marker='.', linestyle='', color='r')[0])
-
-
-        self.ani = animation.FuncAnimation(
-            self.fig, self.animate, init_func=self.init_plot,  interval=500, blit=False, save_count=50)
+        #grd = self.printGrid()
 
 
 
@@ -72,17 +54,11 @@ class Model:
         car.currentVel = max(vtemp-1, 0) if random.random()<p else vtemp
 
     def printGrid(self):
-        grid = [[None for i in range(0, GRIDLEN)] for j in range(LANES+1)]
+        grid = [[None for i in range(0, GRIDLEN)] for j in range(LANES)]
         for j,lane in enumerate(self.traffic):
             for car in lane:
-                if car.id in self.highlightID:
-                    grid[LANES][car.posX - 1] = car.posY
-                else:
-                    grid[j][car.posX - 1] = car.posY
-            # grid[0][car.posX - 1] = car.posY
-            # grid[1][car.posX - 1] = car.posY+1
-        #grid = [list(map(lambda x: None if x== HIGHLIGHT_ID else (0 if x is not None else None), grid[0])),
-        #        list(map(lambda x: None if x == HIGHLIGHT_ID else (1 if x is not None else None), grid[1]))]
+                grid[j][car.posX - 1] = car.posY
+
         return grid
 
     def findNearest(self, destLane, posX):
@@ -146,56 +122,38 @@ class Model:
                 change = self.switchLane(car, self.traffic[i][(j+1)%len(self.traffic[i])], -1)
                 if change == 0:
                     change = self.switchLane(car, self.traffic[i][(j+1)%len(self.traffic[i])], 1)
+                car.laneChange = change
                 car.posY = car.posY + change
                 newTraffic[i+change].append(car)
 
         for i in range (LANES):
             newTraffic[i].sort(key=OrderCars)
-        # newTraffic[0].sort(key=OrderCars)
-        # newTraffic[1].sort(key=OrderCars)
 
         for i in range(LANES):
             for j in range(len(newTraffic[i])):
                 self.updateCarVel(newTraffic[i][j], newTraffic[i][(j+1)%len(newTraffic[i])])
 
-        # firstCar = self.traffic[0]
-        # for i in range(CARNUM - 1):
-        #     self.UpdateCarVel(i, self.traffic[i + 1])
-        # self.UpdateCarVel(-1, firstCar)
-
+        #przesuwanie samochodów
         for lane in newTraffic:
             for e in lane:
                 e.posX = (e.posX + e.currentVel) % GRIDLEN
+                e.updateMarker()
+
 
         self.traffic = newTraffic
-
-        return self.printGrid()
-
-
-    def animate(self,i):
-        ret = self.runSim()
-        # self.line0.set_ydata(ret[0])  # update the data.
-        # self.line1.set_ydata(ret[1])
-        for i in range(LANES+1):
-            self.lines[i].set_ydata(ret[i])
-        return self.lines
-
-    def init_plot(self):
-        # self.line0.set_ydata([None for i in range(0, GRIDLEN)])
-        # self.line1.set_ydata([None for i in range(0, GRIDLEN)])
-        for i in range(LANES):
-            self.lines[i].set_ydata([None for j in range(0, GRIDLEN)])
-        return self.lines
-
-    def start(self):
-        plt.xlim((0, GRIDLEN))
-        plt.ylim((-0.5,20))
-        plt.show()
+        #return self.printGrid()
 
 
+tk = Tk()
+canvas = Canvas(tk, width = 1200, height = 400)
+canvas.pack()
 
-model = Model()
 
-model.start()
+model = Model(tk,canvas)
+
+while True:
+    tk.update()
+    model.runSim()
+    sleep(0.5)
 
 
